11f56b99303997ec0e6ec9f7eded3aca261602c4
Muhamad Fahmi Rahmatullah <fahmi.rahmat07@gmail.com>
2021-12-11 02:01:16 +0700

    Initial project and finished CRUD Category and Nominal collections

---
+++ db/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const { urlDb } = require("../config");
+
+mongoose.connect(urlDb, {
+  useUnifiedTopology: true,
+  //   useFindAndModify: true,
+  //   useCreateIndex: true,
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+
+module.exports = db;
